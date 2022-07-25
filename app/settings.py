1183903b82546f2d5726adb814185d67925a0291
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-07-25 17:43:46 -0300

    Comiti inicial

--- app/settings.py
+++ app/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+import django_on_heroku
 
+django_on_heroku.settings(locals())
 
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-gbq_nax(8q-%@&7qkz$((w!5i-y2lg#7icxytdzm1*^=w#su8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
