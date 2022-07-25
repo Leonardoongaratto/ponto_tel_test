de1e5556015a24ef0c13ae8157c1565cc6c2f575
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-07-25 17:29:56 -0300

    commit

--- app/settings.py
+++ app/settings.py
@@ -14,9 +14,7 @@ from datetime import timedelta
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import django_on_heroku
 
-django_on_heroku.settings(locals())
 
 load_dotenv()
 
